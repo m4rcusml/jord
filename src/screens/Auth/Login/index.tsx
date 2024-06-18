947c9af2a6f16bc9ca72103f1294220641929a4a
fmarcus549 <fmarcus549@gmail.com>
2024-06-18 20:36:51 +0000

    AJUSTES

--- src/screens/Auth/Login/index.tsx
+++ src/screens/Auth/Login/index.tsx
@@ -1,16 +1,48 @@
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+
+import auth from '@react-native-firebase/auth';
+
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from "react-hook-form";
+
+const LoginFormSchema = z.object({
+  email: z.string().email('Insira um email válido'),
+  password: z.string().min(0, 'Insira sua senha'),
+});
+
+type LoginForm = z.infer<typeof LoginFormSchema>;
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile']
+});
 
 export function Login() {
-    return (
-        <SafeAreaView style={styles.container}>
+  const { handleSubmit, control, formState: { errors } } = useForm<LoginForm>({
+    resolver: zodResolver(LoginFormSchema),
+    mode: 'onChange',
+  });
+  
+  async function loginWithGoogle() {
+    try {
+      const { idToken } = await GoogleSignin.signIn();
+      auth().signInWithCredential(auth.GoogleAuthProvider.credential(idToken))
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 })
--- src/screens/Auth/Signup/index.tsx
+++ src/screens/Auth/Signup/index.tsx
@@ -1,16 +1,56 @@
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+
+import auth from '@react-native-firebase/auth';
+
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from "react-hook-form";
+
+const SignupFormSchema = z.object({
+  email: z.string().email('Insira um email válido'),
+  password: z.string().min(0, 'Insira sua senha'),
+  confirmPassword: z.string().min(0, 'Insira sua senha'),
+}).superRefine(({ password, confirmPassword }, ctx) => {
+  if (password !== confirmPassword) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: 'As senhas não conferem',
+    });
+  }
+});
+
+type SignupForm = z.infer<typeof SignupFormSchema>;
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile']
+});
 
 export function Signup() {
-    return (
-        <SafeAreaView style={styles.container}>
+  const { handleSubmit, control, formState: { errors } } = useForm<SignupForm>({
+    resolver: zodResolver(SignupFormSchema),
+    mode: 'onChange',
+  });
 
-        </SafeAreaView>
-    )
+  async function loginWithGoogle() {
+    try {
+      const { idToken } = await GoogleSignin.signIn();
+      auth().signInWithCredential(auth.GoogleAuthProvider.credential(idToken))
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 })
