87898dc508b79c11d830dc70b88dff53780bcf7a
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2024-06-19 21:54:33 -0400

    updates

--- src/screens/Auth/Login/index.tsx
+++ src/screens/Auth/Login/index.tsx
@@ -1,31 +1,33 @@
-import { Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from 'react';
+import { ActivityIndicator, Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { AuthRoutesType } from "../../../routes/auth.routes";
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { AuthRoutesType } from '../../../routes/auth.routes';
 
 import auth from '@react-native-firebase/auth';
 
-import { z } from "zod";
+import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from 'react-hook-form';
 
 import Logo from '../../../assets/logo.png';
 import GoogleLogo from '../../../assets/google-logo.png';
 
 const LoginFormSchema = z.object({
-  email: z.string().email('Insira um email válido'),
-  password: z.string().min(0, 'Insira sua senha'),
+  email: z.string({ message: 'Campo obrigatório' }).email('Insira um email válido'),
+  password: z.string({ message: 'Campo obrigatório' }).min(0, 'Insira sua senha'),
 });
 
 type LoginForm = z.infer<typeof LoginFormSchema>;
 
 GoogleSignin.configure({
   scopes: ['email', 'profile'],
-  webClientId: '227858259368-s0ojvq8jufo8oja1gg0aivuendejjqj6.apps.googleusercontent.com'
+  webClientId: '227858259368-s9d98824oek9ebdu1scjn8svp2unmsjo.apps.googleusercontent.com'
 });
 
 export function Login() {
+  const [isLoading, setIsLoading] = useState(false);
   const { navigate } = useNavigation<NavigationProp<AuthRoutesType>>();
   const { handleSubmit, control, formState: { errors } } = useForm<LoginForm>({
     resolver: zodResolver(LoginFormSchema),
@@ -34,13 +36,25 @@ export function Login() {
 
   async function loginWithGoogle() {
     try {
+      setIsLoading(true);
       GoogleSignin.hasPlayServices();
       const { idToken } = await GoogleSignin.signIn();
       auth().signInWithCredential(auth.GoogleAuthProvider.credential(idToken))
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
     }
   };
+
+  async function handleLogin({email, password}: LoginForm) {
+    try {
+      setIsLoading(true);
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -53,17 +67,34 @@ export function Login() {
       <KeyboardAvoidingView behavior='padding' style={{ gap: 20 }}>
         <View style={styles.textfield}>
           <Text style={styles.text}>E-mail</Text>
-          <TextInput
-            style={styles.input}
-            placeholder='Digite seu email'
-          />
+          <Controller
+            name='email'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                style={styles.input}
+                onChangeText={onChange}
+                placeholder='Digite seu email'
+              />
+            )} />
+          {errors.email?.message && <Text style={[styles.text, { color: 'red', alignSelf: 'flex-end' }]}>{errors.email.message}</Text>}
         </View>
         <View style={styles.textfield}>
           <Text style={styles.text}>Senha</Text>
-          <TextInput
-            style={styles.input}
-            placeholder='Digite sua senha'
-          />
+          <Controller
+            name='password'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                style={styles.input}
+                onChangeText={onChange}
+                placeholder='Digite sua senha'
+                secureTextEntry
+              />
+            )} />
+          {errors.password?.message && <Text style={[styles.text, { color: 'red', alignSelf: 'flex-end' }]}>{errors.password.message}</Text>}
         </View>
       </KeyboardAvoidingView>
 
@@ -73,8 +104,8 @@ export function Login() {
       </TouchableOpacity>
 
       <View style={{ gap: 20 }}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>Entrar</Text>
+        <TouchableOpacity style={styles.button} onPress={handleSubmit(handleLogin)}>
+          <Text style={styles.text}>{isLoading ? <ActivityIndicator color='#02084B' /> : 'Entrar'}</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={() => navigate('signup')}>
           <Text style={styles.text}>Criar uma conta</Text>
--- src/screens/Auth/Signup/index.tsx
+++ src/screens/Auth/Signup/index.tsx
@@ -1,33 +1,35 @@
-import { Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from 'react';
+import { ActivityIndicator, Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { AuthRoutesType } from "../../../routes/auth.routes";
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { AuthRoutesType } from '../../../routes/auth.routes';
 
 import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
 
-import { z } from "zod";
+import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from 'react-hook-form';
 
 import Logo from '../../../assets/logo.png';
 import GoogleLogo from '../../../assets/google-logo.png';
 
-
 const SignupFormSchema = z.object({
-  username: z.string().min(0, 'Insira seu nome'),
-  email: z.string().email('Insira um email válido'),
-  password: z.string().min(0, 'Insira sua senha'),
+  username: z.string({ message: 'Campo obrigatório' }).min(0, 'Insira seu nome'),
+  email: z.string({ message: 'Campo obrigatório' }).email('Insira um email válido'),
+  password: z.string({ message: 'Campo obrigatório' }).min(0, 'Insira sua senha'),
 });
 
 type SignupForm = z.infer<typeof SignupFormSchema>;
 
 GoogleSignin.configure({
   scopes: ['email', 'profile'],
-  webClientId: '227858259368-s0ojvq8jufo8oja1gg0aivuendejjqj6.apps.googleusercontent.com'
+  webClientId: '227858259368-s9d98824oek9ebdu1scjn8svp2unmsjo.apps.googleusercontent.com'
 });
 
 export function Signup() {
+  const [isLoading, setIsLoading] = useState(false);
   const { navigate } = useNavigation<NavigationProp<AuthRoutesType>>();
   const { handleSubmit, control, formState: { errors } } = useForm<SignupForm>({
     resolver: zodResolver(SignupFormSchema),
@@ -36,12 +38,31 @@ export function Signup() {
 
   async function loginWithGoogle() {
     try {
+      GoogleSignin.hasPlayServices();
       const { idToken } = await GoogleSignin.signIn();
       auth().signInWithCredential(auth.GoogleAuthProvider.credential(idToken))
     } catch (error) {
       console.log(error);
     }
   };
+
+  async function handleSignup({ username, email, password }: SignupForm) {
+    try {
+      setIsLoading(true);
+      auth().createUserWithEmailAndPassword(email, password).then(credentials => {
+        database()
+          .ref(`/users/${credentials.user.uid}`)
+          .set({
+            username,
+          })
+          .then(() => console.log('Data set.'));
+      });
+
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -54,30 +75,55 @@ export function Signup() {
       <KeyboardAvoidingView behavior='padding' style={{ gap: 20 }}>
         <View style={styles.textfield}>
           <Text style={styles.text}>Nome de usuário</Text>
-          <TextInput
-            style={styles.input}
-            placeholder='Digite seu nome'
-          />
+          <Controller
+            name='username'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                style={styles.input}
+                onChangeText={onChange}
+                placeholder='Digite seu nome'
+              />
+            )} />
+          {errors.username?.message && <Text style={[styles.text, { color: 'red', alignSelf: 'flex-end' }]}>{errors.username.message}</Text>}
         </View>
         <View style={styles.textfield}>
           <Text style={styles.text}>E-mail</Text>
-          <TextInput
-            style={styles.input}
-            placeholder='Digite seu email'
-          />
+          <Controller
+            name='email'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                style={styles.input}
+                onChangeText={onChange}
+                placeholder='Digite seu email'
+              />
+            )} />
+          {errors.email?.message && <Text style={[styles.text, { color: 'red', alignSelf: 'flex-end' }]}>{errors.email.message}</Text>}
         </View>
         <View style={styles.textfield}>
           <Text style={styles.text}>Senha</Text>
-          <TextInput
-            style={styles.input}
-            placeholder='Digite sua senha'
-          />
+          <Controller
+            name='password'
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                value={value}
+                style={styles.input}
+                onChangeText={onChange}
+                placeholder='Digite sua senha'
+                secureTextEntry
+              />
+            )} />
+          {errors.password?.message && <Text style={[styles.text, { color: 'red', alignSelf: 'flex-end' }]}>{errors.password.message}</Text>}
         </View>
       </KeyboardAvoidingView>
 
       <View style={{ gap: 20 }}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>Criar conta</Text>
+        <TouchableOpacity style={styles.button} onPress={handleSubmit(handleSignup)}>
+          <Text style={styles.text}>{isLoading ? <ActivityIndicator color='#02084B' /> : 'Criar conta'}</Text>
         </TouchableOpacity>
       </View>
 
